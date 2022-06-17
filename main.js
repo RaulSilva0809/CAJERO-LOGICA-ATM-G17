9410d6d6e6575b47445238d833e16c2d6508d7bb
RaulSilva0809 <tecnosis08@gmail.com>
2022-06-17 14:33:11 -0500

    Comienzo de la logica

---
+++ main.js
@@ -0,0 +1,12 @@
+ var cuentas = [
+   { nombre: "Hiromi", saldo: 200, password: 'helloworld' },
+   { nombre: "Luis", saldo: 290, password: 'l33t' },
+   { nombre: "Carlos", saldo: 67, password: '123' }
+ ];
+
+
+ sesion()
+
+ function sesion()
+ let name = prompt('Introduce nombre:');
+ let password = prompt('Introduce password')
